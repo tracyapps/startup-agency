b76863ca562af4592e7417fd335dbdcb6e824903
tapps <t@thisroad.org>
2021-01-05 20:11:57 -0600

    css vars in place, default theme import

---
+++ assets/css/css-theme-var-imports/default-theme.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * CSS variable import: default theme
+ *
+ */ ?>
+
+<style rel="stylesheet" type="text/css">
+	:root {
+		--header_background_color:		#fff;
+		--header_text_color:			#1864ab;
+		--top_nav_bar_background:		transparent;
+		--nav_item_background_color:	#d0ebff;
+		--nav_item_text_color:			#1864ab;
+		--nav_item_hover_background_color: #1864ab;
+		--nav_item_hover_text_color:	#d0ebff;
+		--nav_item_font:				'Work Sans', sans-serif;
+
+		--page_background_color:		#fff;
+		--page_text_color:				#212529;
+		--page_link_color:				#1864ab;
+		--page_link_hover_color:		#5f3dc4;
+		--body_text_font:				'Work Sans', sans-serif;
+		--body_text_default_size:		21px;
+		--page_heading_text_font:		'Josefin Slab', serif;
+		--page_heading_text_font_weight: light;
+
+		--primary_button_background_color: #d9480f;
+		--primary_button_text_color:	#fff;
+		--primary_button_hover_background_color: #a61e4d;
+		--primary_button_hover_text_color: #fff;
+
+		--secondary_button_background_color: #4dabf7;
+		--secondary_button_text_color:	#fff;
+		--secondary_button_hover_background_color: #20c997;
+		--secondary_button_hover_text_color: #fff;
+
+		--footer_background_color:		#212529;
+		--footer_text_color:			#ced4da;
+		--footer_link_color:			#d0ebff;
+		--footer_link_hover_color:		#ced4da;
+	}
+
+</style>
--- header.php
+++ header.php
@@ -24,14 +24,7 @@
 
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<style type="text/css" rel="stylesheet">
-		:root {
-			--header-background-color: #0f0;
-			--header-nav-background: #f00;
-			--transition-speed: 300ms;
-			--max-page-width: 900px;
-		}
-	</style>
+	<?php include( 'assets/css/css-theme-var-imports/default-theme.php' ); ?>
 	<?php wp_head(); ?>
 
 
